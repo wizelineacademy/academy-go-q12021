e938ec4ff3218280d19082cbf43d4e6a1cfdce43
Alexis Aguirre <aaaguirrer@hotmail.com>
2020-11-17 23:50:26 -0600

    Deleted unused code

--- infraestructure/router/router.go
+++ infraestructure/router/router.go
@@ -5,19 +5,7 @@ import (
 )
 
 //NewRouter creates a new router instance
-/*
-func NewRouter(e *echo.Echo, c controller.AppController) *echo.Echo {
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	e.GET("/users", func(context echo.Context) error { return c.GetUsers(context) })
-
-	return e
-}
-*/
-
-//NewRouter creates a new router instance
 func NewRouter() *mux.Router {
-	router := mux.NewRouter()
+	router := mux.NewRouter().StrictSlash(true)
 	return router
 }
