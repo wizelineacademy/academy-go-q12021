70faf312e5df207269420a959bceddb178790c53
Hiram Torres Lopez <hiram@jtl.mx>
2020-11-21 23:53:59 -0700

    Add Repository definition

---
+++ repository/repository.go
@@ -0,0 +1,22 @@
+package repository
+
+import "github.com/javiertlopez/golang-bootcamp-2020/model"
+
+// EventRepository interface
+type EventRepository interface {
+	// Event related
+	Create(event model.Event) (model.Event, error)
+	GetByID(id string) (model.Event, error)
+	GetAll() ([]model.Event, error)
+	Update(event model.Event) (model.Event, error)
+	Delete(id string) error
+}
+
+// ReservationRepository interface
+type ReservationRepository interface {
+	// Reservation related
+	Create(reservation model.Reservation) (model.Reservation, error)
+
+	// Related to Event
+	GetByEventID(id string) ([]model.Reservation, error)
+}
