07401a6b3363a1a49562e2390c2d4fb9a46b7ac8
MiguelAGrover <50057779+MiguelAGrover@users.noreply.github.com>
2020-11-21 19:20:31 -0600

    Update digimon_repository.go
    Complete comments of what interfaces and functions mean.

--- interface/repository/digimon_repository.go
+++ interface/repository/digimon_repository.go
@@ -10,17 +10,17 @@ type digimonRepository struct {
 	db *gorm.DB
 }
 
-// DigimonRepository
+// DigimonRepository shows all the methods to be implemented by a digimon repository
 type DigimonRepository interface {
 	FindAll(d []*model.Digimon) ([]*model.Digimon, error)
 }
 
-// NewDigimonRepository
+// NewDigimonRepository Returns an instance of a digimon repository
 func NewDigimonRepository(db *gorm.DB) DigimonRepository {
 	return &digimonRepository{db}
 }
 
-// FindAll
+// FindAll retrieve all the digimons from the database
 func (dr *digimonRepository) FindAll(d []*model.Digimon) ([]*model.Digimon, error) {
 	err := dr.db.Find(&d).Error
 
