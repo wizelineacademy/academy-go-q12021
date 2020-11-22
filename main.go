5dad1f8d5338daa4b4ec442f80f524159202778f
Rodolfo Martínez Vega <rodomar@outlook.com>
2020-11-22 13:32:58 -0600

    Clean code

--- main.go
+++ main.go
@@ -6,23 +6,11 @@ import (
 
 	"github.com/ramrodo/golang-bootcamp-2020/config"
 	"github.com/ramrodo/golang-bootcamp-2020/usecase/repository"
-	// "github.com/ramrodo/golang-bootcamp-2020/usecase/repository"
 )
 
 func main() {
 	config.ReadConfig()
 	// fmt.Printf("Server listen at %s:%s\n", config.C.Server.URL, config.C.Server.Port)
-
-	// This returns error: "panic: runtime error: invalid memory address or nil pointer dereference"
-	// var interactor interactor.FilmInteractor
-	// repository := interactor.FilmRepository
-	// controller := FilmController
-	// films, err := repository.FindAll()
-	// 	films, err := interactor.Get()
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 	}
-	// fmt.Println("films:", films)
 
 	all, err := repository.FindAll()
 
