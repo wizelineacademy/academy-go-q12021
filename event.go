b26da78ace5c17bf9dcec2df10260bf9f967faf3
Hiram Torres Lopez <hiram@jtl.mx>
2020-11-21 23:02:32 -0700

    Add Event definition

---
+++ event.go
@@ -0,0 +1,48 @@
+package main
+
+import "time"
+
+// Event holds information related to an event
+type Event struct {
+	ID            string     `json:"id"`
+	Description   string     `json:"description"`
+	Type          string     `json:"type"` // how can I predefine values here?
+	Status        string     `json:"status"`
+	CreatedAt     *time.Time `json:"created_at"`
+	UpdatedAt     *time.Time `json:"updated_at"`
+	EventDate     *time.Time `json:"event_date"`     // should I drop 'event_'?
+	EventLocation string     `json:"event_location"` // should I drop 'event_'?
+
+	// Customer information
+	Name  string `json:"name"`
+	Phone string `json:"phone"`
+	Email string `json:"email"`
+
+	// Guests
+	Reservations []Reservation `json:"reservations"`
+	TotalFee     float64       `json:"total_fee"` // should I include this? There's a func for this
+}
+
+// totalFee returns the total cost of the event guests
+func (e *Event) totalFee() float64 {
+	var total float64
+
+	for _, reservation := range e.Reservations {
+		total += reservation.totalFee()
+	}
+
+	return total
+}
+
+// filterReservations returns reservations filtered by status
+func (e *Event) filterReservations(status string) []Reservation {
+	var list []Reservation
+
+	for _, reservation := range e.Reservations {
+		if e.Status == status {
+			list = append(list, reservation)
+		}
+	}
+
+	return list
+}
