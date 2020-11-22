ed415b323c1f9f56bedbbea22f22cb68b9a3b38e
Rodolfo Martínez Vega <rodomar@outlook.com>
2020-11-22 13:30:51 -0600

    Load CSV file and return the model data

---
+++ model/film.go
@@ -0,0 +1,12 @@
+package model
+
+// Film - definition
+type Film struct {
+	ID          string
+	Title       string
+	Description string
+	Director    string
+	Producer    string
+	ReleaseDate string
+	RtScore     string
+}
---
+++ usecase/repository/film_repository.go
@@ -0,0 +1,51 @@
+package repository
+
+import (
+	"bufio"
+	"encoding/csv"
+	"os"
+
+	"github.com/ramrodo/golang-bootcamp-2020/model"
+)
+
+// FilmRepository - interface
+type FilmRepository interface {
+	FindAll() ([]model.Film, error)
+}
+
+// FindAll - function
+func FindAll() ([]model.Film, error) {
+	films := []model.Film{}
+
+	csvFile, err := os.Open("config/ghibliDB.csv")
+
+	if err != nil {
+		return nil, err
+	}
+
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+
+	data, err := reader.ReadAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	for i, row := range data {
+		if i == 0 {
+			continue
+		}
+
+		films = append(films, model.Film{
+			ID:          row[0],
+			Title:       row[1],
+			Description: row[2],
+			Director:    row[3],
+			Producer:    row[4],
+			ReleaseDate: row[5],
+			RtScore:     row[6],
+		})
+	}
+
+	return films, nil
+}
