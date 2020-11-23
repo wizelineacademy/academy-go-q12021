39e32fef6f035301460e4f23d4706368b2c7be91
Fausto Salazar <fsalazar@brightcove.com>
2020-11-22 22:05:50 -0600

    Allow to pass some data

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import(
     "net/http"
     "log"
+    "io/ioutil"
 )
 
 func main() {
-    http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         log.Println("Hello World")
+        d, _ := ioutil.ReadAll(r.Body)
+
+        log.Printf("Data %s\n", d)
     }) 
 
     http.ListenAndServe(":9090", nil)
