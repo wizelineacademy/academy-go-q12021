e2461fcc986666ebfdf188e08508ae25efdfee81
Juan Pablo <pmartinez@solutionreach.com>
2020-11-22 18:09:33 -0600

    add layers to main with router

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
-package golang_bootcamp_2020
+package main
+
+import (
+	"context"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/pankecho/golang-bootcamp-2020/store"
+	"github.com/pankecho/golang-bootcamp-2020/transport"
+	"github.com/pankecho/golang-bootcamp-2020/usecase"
+)
+
+func main() {
+	ctx := context.Background()
+	personStore, _ := store.NewStore(ctx)
+	personUseCase := usecase.NewPerson(personStore)
+	personTransport := transport.NewPerson(personUseCase)
+
+	e := transport.NewRouter(personTransport)
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
