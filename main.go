10cd075057446ab26925a86a872a1b30a9d3e02c
Fausto Salazar <fsalazar@brightcove.com>
2020-11-22 21:57:30 -0600

    simple hello world

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "net/http"
+import(
+    "net/http"
+    "log"
+)
 
 func main() {
+    http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+        log.Println("Hello World")
+    }) 
+
     http.ListenAndServe(":9090", nil)
 }
