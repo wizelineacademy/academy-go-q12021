0eb447b6c2f3a67c47626b1ce4c1923a4815e1e7
Hiram Torres Lopez <hiram@jtl.mx>
2020-11-22 22:31:06 -0700

    Add usecase implementation

--- usecase/events.go
+++ usecase/events.go
@@ -1,6 +1,11 @@
 package usecase
 
-import "github.com/javiertlopez/golang-bootcamp-2020/model"
+import (
+	"fmt"
+
+	"github.com/javiertlopez/golang-bootcamp-2020/model"
+	"github.com/javiertlopez/golang-bootcamp-2020/repository"
+)
 
 // Events interface
 type Events interface {
@@ -16,4 +21,48 @@ type Events interface {
 	GetReservations(id string) ([]model.Reservation, error)
 
 	// Do I need to pass the context?
+}
+
+type events struct {
+	eventRepo       repository.EventRepository
+	reservationRepo repository.ReservationRepository
+}
+
+// NewEventUseCase returns the usecase implementation
+func NewEventUseCase(
+	eventRepo repository.EventRepository,
+	reservationRepo repository.ReservationRepository,
+) Events {
+	return &events{
+		eventRepo,
+		reservationRepo,
+	}
+}
+
+func (e *events) Create(event model.Event) (model.Event, error) {
+	return model.Event{}, fmt.Errorf("not implemented")
+}
+
+func (e *events) GetByID(id string) (model.Event, error) {
+	return model.Event{}, fmt.Errorf("not implemented")
+}
+
+func (e *events) GetAll() ([]model.Event, error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+func (e *events) Update(event model.Event) (model.Event, error) {
+	return model.Event{}, fmt.Errorf("not implemented")
+}
+
+func (e *events) Delete(id string) error {
+	return fmt.Errorf("not implemented")
+}
+
+func (e *events) AddReservations(id string, reservations []model.Reservation) ([]model.Reservation, error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+func (e *events) GetReservations(id string) ([]model.Reservation, error) {
+	return nil, fmt.Errorf("not implemented")
 }
