e7dcc381124dc88cf185f02370d2f3c85042958e
Erick G. Lopez Siordia <erick.lopez@wizeline.com>
2020-12-02 13:27:37 -0600

    fixed missing spaces after double slash

--- entity/quote-garden.go
+++ entity/quote-garden.go
@@ -1,6 +1,6 @@
 package entity
 
-//QuoteGarden represents the response from https://quote-garden.herokuapp.com/api/v2/quotes/random
+// QuoteGarden represents the response from https://quote-garden.herokuapp.com/api/v2/quotes/random
 type QuoteGarden struct {
 	StatusCode int   `json:"statusCode"`
 	Quote      Quote `json:"quote"`
