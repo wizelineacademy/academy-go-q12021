13372e276e8c3ebbf735610168017084d97457f1
Erick G. Lopez Siordia <erick.lopez@wizeline.com>
2020-12-14 13:34:12 -0600

    populated quote handler test tables; removed quote handler blank and repeated test cases;

--- service/handler/quote_test.go
+++ service/handler/quote_test.go
@@ -4,12 +4,9 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"reflect"
-	"strings"
 	"testing"
 
 	"github.com/etyberick/golang-bootcamp-2020/entity"
-	"github.com/etyberick/golang-bootcamp-2020/service/repository"
 	"github.com/etyberick/golang-bootcamp-2020/usecase/interactor"
 )
 
@@ -37,124 +34,64 @@ func TestUpdate(t *testing.T) {
 	}
 }
 
-// Test when fetching from an empty database
-func TestGetAllEmptyDatabase(t *testing.T) {
-	// Mock a new request
+func TestGet(t *testing.T) {
+	// We need to set some variables before starting the test tables
 	req, err := http.NewRequest("GET", "/v0/quote", nil)
 	if err != nil {
 		t.Errorf("failed mocking request, %v", err.Error())
 	}
-	// Initialize interactor and pass it to the handler
-	c := &entity.Config{
-		CSVFilepath: "empty.csv",
-		Port:        "8080",
-	}
-	qi := interactor.NewQuoteInteractor(*c)
-	q := NewQuote(qi)
-	// Execute request and check result
-	rec := httptest.NewRecorder()
-	q.GetAll(rec, req)
-	b, err := ioutil.ReadAll(rec.Body)
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-	if strings.Compare(strings.TrimSpace(string(b)), repository.EmptyDatabase) != 0 {
-		t.FailNow()
-	}
-}
-
-// Test when fetching from a database recently populated
-func TestGetAll(t *testing.T) {
-	t.Run("TestUpdate", TestUpdate)
-	// Mock a new request
-	req, err := http.NewRequest("GET", "/v0/quote", nil)
-	if err != nil {
-		t.Errorf("failed mocking request, %v", err.Error())
-	}
-	// Initialize interactor and pass it to the handler
-	c := &entity.Config{
-		CSVFilepath: "test.csv",
-		Port:        "8080",
-	}
-	qi := interactor.NewQuoteInteractor(*c)
-	q := NewQuote(qi)
-	// Execute request and check result
-	rec := httptest.NewRecorder()
-	q.GetAll(rec, req)
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-	if rec.Result().StatusCode != http.StatusOK {
-		t.FailNow()
-	}
-}
-
-func TestNewQuote(t *testing.T) {
+	// Initialize test tables
 	type args struct {
 		qi interactor.QuoteInteractor
 	}
 	tests := []struct {
-		name string
-		args args
-		want Quote
+		args             args
+		name             string
+		pretest          func(t *testing.T)
+		request          *http.Request
+		responseRecorder *httptest.ResponseRecorder
 	}{
-		// TODO: Add test cases.
+		// Empty database case
+		{
+			args: args{
+				qi: interactor.NewQuoteInteractor(entity.Config{
+					CSVFilepath: "empty.csv",
+					Port:        "8080",
+				}),
+			},
+			name:             "empty database get",
+			pretest:          nil,
+			request:          req,
+			responseRecorder: httptest.NewRecorder(),
+		},
+		// Populated database case
+		{
+			args: args{
+				qi: interactor.NewQuoteInteractor(entity.Config{
+					CSVFilepath: "test.csv",
+					Port:        "8080",
+				}),
+			},
+			name:             "populated database GET",
+			pretest:          TestUpdate,
+			request:          req,
+			responseRecorder: httptest.NewRecorder(),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := NewQuote(tt.args.qi); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewQuote() = %v, want %v", got, tt.want)
+			// Perform pretest conditions
+			if tt.pretest != nil {
+				t.Run("Pre-test condition", tt.pretest)
 			}
-		})
-	}
-}
-
-func Test_quote_Update(t *testing.T) {
-	type fields struct {
-		quoteInteractor interactor.QuoteInteractor
-	}
-	type args struct {
-		w http.ResponseWriter
-		r *http.Request
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			q := &quote{
-				quoteInteractor: tt.fields.quoteInteractor,
+			NewQuote(tt.args.qi).GetAll(tt.responseRecorder, tt.request)
+			b, err := ioutil.ReadAll(tt.responseRecorder.Body)
+			if err != nil {
+				t.Errorf(err.Error())
 			}
-			q.Update(tt.args.w, tt.args.r)
-		})
-	}
-}
-
-func Test_quote_GetAll(t *testing.T) {
-	type fields struct {
-		quoteInteractor interactor.QuoteInteractor
-	}
-	type args struct {
-		w http.ResponseWriter
-		r *http.Request
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			q := &quote{
-				quoteInteractor: tt.fields.quoteInteractor,
+			if len(b) == 0 {
+				t.Errorf("GET = %q, want length different from 0", string(b))
 			}
-			q.GetAll(tt.args.w, tt.args.r)
 		})
 	}
 }
