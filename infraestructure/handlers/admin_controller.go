feb82d348e4043619cf2bf98b8b42c17c350dd90
Alexis Aguirre <aaaguirrer@hotmail.com>
2020-12-13 21:04:07 -0600

    Removed unnecesary logs

--- infraestructure/handlers/admin_controller.go
+++ infraestructure/handlers/admin_controller.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"log"
 	"net/http"
 	"time"
 
@@ -12,7 +11,6 @@ import (
 //GetLogs is the handler for retrieving the stored logs in a period of time
 // and returning it as json
 func GetLogs(w http.ResponseWriter, r *http.Request) {
-	log.Println("Get logs handler")
 	ai := registry.NewAdminInteractor()
 	logs, err := ai.GetLogs("users", time.Time{}, time.Time{})
 	if err != nil {
@@ -20,8 +18,6 @@ func GetLogs(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Println(datastore.UserLogToJson(logs))
-	//body, err := json.Marshal(logs)
 	if err != nil {
 		writeError(w, r, http.StatusInternalServerError, "")
 		return
