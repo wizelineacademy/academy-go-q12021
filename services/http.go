55feb73e20289a0236c5f1d85ce0b0dfdcadea80
Jesusfer2575 <ing.adrian94fdz@gmail.com>
2021-02-22 23:13:19 -0600

    defering response after checking errors

--- services/http.go
+++ services/http.go
@@ -31,12 +31,16 @@ func (h *Http) GetPokemonsFromExternalAPI() {
 	req.Header.Add("Content-Type", "application/json")
 
 	resp, err := client.Do(req)
-
+	if err != nil {
+		fmt.Print(err.Error())
+	}
 	defer resp.Body.Close()
+
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Print(err.Error())
 	}
+
 	var response models.PokemonExternal
 	json.Unmarshal(bodyBytes, &response)
 
