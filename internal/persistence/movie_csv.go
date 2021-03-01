6a393a4bbb2eef63e022e6e65db81f1113bafc41
A Ruiz <luis.alonso.16@hotmail.com>
2021-03-01 01:09:32 -0700

    Add Movie CSV fetching

--- internal/persistence/movie_csv.go
+++ internal/persistence/movie_csv.go
@@ -70,7 +70,47 @@ func (m *MovieCSV) searchMovieOnFile(r *csv.Reader, id valueobject.MovieID) (*ag
 func (m *MovieCSV) Search(ctx context.Context, criteria repository.Criteria) ([]*aggregate.Movie, string, error) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
-	return nil, "", nil
+
+	file, err := os.Open(m.cfg.MoviesDataset)
+	if err != nil {
+		return nil, "", err
+	}
+	return m.searchMoviesOnFile(csv.NewReader(file), criteria)
+}
+
+func (m *MovieCSV) searchMoviesOnFile(r *csv.Reader, criteria repository.Criteria) ([]*aggregate.Movie, string, error) {
+	isHeader := true
+	totalCount := 0
+	allowFetch := false
+	movies := make([]*aggregate.Movie, 0)
+	nextPageToken := ""
+	for {
+		records, err := r.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, "", err
+		} else if isHeader {
+			isHeader = false
+			continue
+		} else if criteria.NextPage != "" && records[0] != criteria.NextPage && allowFetch == false {
+			continue
+		}
+
+		movie := aggregate.NewEmptyMovie()
+		if err = marshal.UnmarshalMovieCSV(movie, records...); err != nil {
+			return nil, "", err
+		} else if totalCount >= criteria.Limit { // fetch one more item to get next page
+			nextPageToken = string(movie.ID)
+			break
+		}
+
+		movies = append(movies, movie)
+		totalCount++
+		allowFetch = true
+	}
+
+	return movies, nextPageToken, nil
 }
 
 // Save stores the current state of the given Movie
