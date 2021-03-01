de5cbffa2a222ea33a1b4b827781fc141e4ea70b
A Ruiz <luis.alonso.16@hotmail.com>
2021-03-01 02:30:25 -0700

    Add feature to read multiple directors on a same film

--- internal/event/movie_created.go
+++ internal/event/movie_created.go
@@ -3,6 +3,7 @@ package event
 import (
 	"time"
 
+	"github.com/maestre3d/academy-go-q12021/internal/service"
 	"github.com/maestre3d/academy-go-q12021/internal/valueobject"
 )
 
@@ -10,17 +11,17 @@ import (
 type MovieCreated struct {
 	ID          string    `json:"movie_id"`
 	DisplayName string    `json:"display_name"`
-	Director    string    `json:"director"`
+	Directors   []string  `json:"director"`
 	ReleaseYear int       `json:"release_year"`
 	CreateTime  time.Time `json:"create_time"`
 }
 
-func NewMovieCreated(id valueobject.MovieID, name valueobject.DisplayName, director valueobject.DisplayName,
-	year valueobject.ReleaseYear) MovieCreated {
+func NewMovieCreated(id valueobject.MovieID, name valueobject.DisplayName,
+	year valueobject.ReleaseYear, directors ...valueobject.DisplayName) MovieCreated {
 	return MovieCreated{
 		ID:          string(id),
 		DisplayName: string(name),
-		Director:    string(director),
+		Directors:   service.MarshalDirectorsPrimitive(directors...),
 		ReleaseYear: int(year),
 		CreateTime:  time.Now().UTC(),
 	}
--- internal/marshal/movie_csv.go
+++ internal/marshal/movie_csv.go
@@ -2,6 +2,7 @@ package marshal
 
 import (
 	"strconv"
+	"strings"
 
 	"github.com/maestre3d/academy-go-q12021/internal/aggregate"
 	"github.com/maestre3d/academy-go-q12021/internal/domain"
@@ -11,7 +12,10 @@ import (
 // ErrCannotParseMovie the given movie could not get parsed
 var ErrCannotParseMovie = domain.NewInfrastructure("cannot parse movie")
 
-const movieTotalFields = 4
+const (
+	movieTotalFields               = 4
+	movieDirectorsDelimiterPattern = ","
+)
 
 // UnmarshalMovieCSV parses the given csv data into a Movie
 func UnmarshalMovieCSV(movie *aggregate.Movie, records ...string) (err error) {
@@ -20,11 +24,21 @@ func UnmarshalMovieCSV(movie *aggregate.Movie, records ...string) (err error) {
 	}
 	movie.ID = valueobject.MovieID(records[0])
 	movie.DisplayName = valueobject.DisplayName(records[1])
-	movie.Director = valueobject.DisplayName(records[2])
+	movie.Directors = unmarshalMovieDirectorsCSV(records[2])
 	year, _ := strconv.Atoi(records[3])
 	movie.ReleaseYear, err = valueobject.NewReleaseYear(year) // avoid integer overflow at runtime
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func unmarshalMovieDirectorsCSV(v string) []valueobject.DisplayName {
+	directors := make([]valueobject.DisplayName, 0)
+	splitDirectors := strings.Split(v, movieDirectorsDelimiterPattern)
+	for _, d := range splitDirectors {
+		directors = append(directors, valueobject.DisplayName(d))
+	}
+
+	return directors
 }
