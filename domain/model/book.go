1a678833e663a5d9cf8552a2cf3d4d7a8e0d1022
“lbswl” <liliana.badillo@wizeline.com>
2021-03-03 11:06:19 -0600

    Move function to book model

--- domain/model/book.go
+++ domain/model/book.go
@@ -8,3 +8,15 @@ type Book struct {
 	Year     int    `json:"yer"`
 	ImageURL string `json:"imageURL"`
 }
+
+// FindBookByID returns the index that corresponds to an ID (if exists)
+func FindBookByID(books []Book, ID int) int {
+
+	for index, item := range books {
+		if item.ID == ID {
+			return index
+		}
+	}
+
+	return -1
+}
--- main.go
+++ main.go
@@ -12,45 +12,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// findBookByID returns the index that corresponds to an ID (if exists)
-func findBookByID(books []model.Book, ID int) int {
-
-	for index, item := range books {
-		if item.ID == ID {
-			return index
-		}
-	}
-
-	return -1
-}
-
-func getBooks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(books)
-}
-
-func getBook(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // Get params
-
-	id, errConv := strconv.Atoi(params["id"])
-
-	if errConv != nil {
-		log.Fatal(errConv)
-	}
-
-	index := findBookByID(books, id)
-
-	if index > -1 {
-		json.NewEncoder(w).Encode(books[index])
-		return
-	}
-	json.NewEncoder(w).Encode(books)
-
-}
-
-// Data
 var books []model.Book
 
 func main() {
@@ -61,9 +22,37 @@ func main() {
 	books = datastore.LoadData(books, "data/books.csv")
 
 	// Route Handlers / Endpoints
-	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", GetBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", GetBooks).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
+
+// GetBooks returns all books
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// GetBook returns a book given its ID
+func GetBook(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Get params
+
+	id, errConv := strconv.Atoi(params["id"])
+
+	if errConv != nil {
+		log.Fatal(errConv)
+	}
+
+	index := model.FindBookByID(books, id)
+
+	if index > -1 {
+		json.NewEncoder(w).Encode(books[index])
+		return
+	}
+	json.NewEncoder(w).Encode(books)
 
 }
