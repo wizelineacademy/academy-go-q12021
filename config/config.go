1618e1a9cd9e2313c8ab79b55d1563222f42eb98
Grethel Bello Cagnant <grethel.bello@wizeline.com>
2021-03-12 16:03:13 -0600

    feat: add config test

--- config/config.go
+++ config/config.go
@@ -3,35 +3,42 @@ package config
 import (
 	"errors"
 	"os"
+
+	"github.com/wizelineacademy/academy-go/constant"
 )
 
-var ServerPort = getServerPort()
-var PokemonSource = getPokemonSource()
+// Environment contains the value of the current environment
 var Environment = getEnv()
 
+// PokemonSource contains the value of data source for pokemon endpoint
+var PokemonSource = getPokemonSource()
+
+// ServerPort contains the value of server port
+var ServerPort = getServerPort()
+
 func getServerPort() string {
-	serverPort := os.Getenv("SERVER_PORT")
-	if len(serverPort) > 0 {
+	serverPort, ok := os.LookupEnv(constant.ServerPortVarName)
+	if ok {
 		return serverPort
 	}
 
-	return "8080"
+	return constant.DefaultServerPort
 }
 
 func getPokemonSource() string {
-	pokemonSource := os.Getenv("POKEMON_SOURCE")
-	if len(pokemonSource) > 0 {
+	pokemonSource, ok := os.LookupEnv(constant.PokemonSourceVarName)
+	if ok {
 		return pokemonSource
 	}
 
-	panic(errors.New("POKEMON_SOURCE env var is required"))
+	panic(errors.New(constant.PokemonSourceVarName + " env var is required"))
 }
 
 func getEnv() string {
-	environment := os.Getenv("ENVIRONMENT")
-	if len(environment) > 0 {
+	environment, ok := os.LookupEnv(constant.EnvironmentVarName)
+	if ok {
 		return environment
 	}
 
-	return "dev"
+	return constant.DefaultEnvironment
 }
