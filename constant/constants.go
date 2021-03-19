659158b18e6d31b13f46196e2bbcf1266cf460cd
Grethel Bello Cagnant <grethel.bello@wizeline.com>
2021-03-18 18:47:41 -0600

    doc: document constants

--- constant/constants.go
+++ constant/constants.go
@@ -9,8 +9,8 @@ const PokemonSourceVarName = "POKEMON_SOURCE"
 // ServerPortVarName is the name used for the environment variable with the value for the server port
 const ServerPortVarName = "SERVER_PORT"
 
-// DefaultEnvironment contains the default value when the env var ENVIRONMENT is not present
-const DefaultEnvironment = "dev"
+// DefaultEnvironment contains the default value when the env var 'ENVIRONMENT' is not present
+const DefaultEnvironment = "test"
 
-// DefaultServerPort contains the default value when the env var SERVER_PORT is not present
+// DefaultServerPort contains the default value when the env var 'SERVER_PORT' is not present
 const DefaultServerPort = "8080"
