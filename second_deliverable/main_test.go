f36ec2dfe2a19cb1bc132fa59dfdb9f8ef56a0d3
Ivan Ibarra Pacheco <bytepacheco@gmail.com>
2021-03-26 16:31:28 -0600

    update files

--- second_deliverable/main_test.go
+++ second_deliverable/main_test.go
@@ -1,49 +1,37 @@
 package main
 
 import (
-	"net/http"
-	"reflect"
 	"testing"
 )
 
-func TestGetData(t *testing.T) {
-	tests := []struct {
-		name      string
-		wantItems []Item
-	}{
-		{
-			name: "Name",
-			wantItems: []Item{
-				{
-					Id:    "1",
-					Title: "1",
-				},
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if gotItems := GetData(); !reflect.DeepEqual(gotItems, tt.wantItems) {
-				t.Errorf("GetData() = %v, want %v", gotItems, tt.wantItems)
-			}
-		})
-	}
+var Items []Item = []Item{
+	{Id: "1", Title: "Netflix"},
+	{Id: "2", Title: "Dispney+"},
+	{Id: "3", Title: "HBO Max"},
+	{Id: "4", Title: "Paramount+"},
+	{Id: "5", Title: "Universal+"},
 }
 
-func TestRenderItem(t *testing.T) {
+func TestWriteDataToCSVFile(t *testing.T) {
 	type args struct {
-		w http.ResponseWriter
-		r *http.Request
+		fileName string
+		items    []Item
 	}
 	tests := []struct {
 		name string
 		args args
 	}{
-		
+		{
+			name: "",
+			args: args{
+				fileName: "test.csv", 
+				items: Items,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			RenderItem(tt.args.w, tt.args.r)
+			WriteDataToCSVFile(tt.args.fileName, tt.args.items)
 		})
 	}
 }
