1ced89c77554734b0509fa14ec52749b62b6c15f
Ivan Ibarra Pacheco <bytepacheco@gmail.com>
2021-03-26 17:05:15 -0600

    added unit test to check if csv was generated succesfully

--- second_deliverable/main_test.go
+++ second_deliverable/main_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
 	"testing"
 )
 
@@ -12,6 +16,36 @@ var Items []Item = []Item{
 	{Id: "5", Title: "Universal+"},
 }
 
+func GetDataFromCSVFile(filePath string) ([][] string)  {
+	csvFile, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("\n- An error ocurred while reading the file. \n", err)
+	} else {
+		fmt.Println("\nSuccessfully Opened CSV file") 
+	}
+	csvLines, err := csv.NewReader(csvFile).ReadAll()
+    if err != nil {
+		log.Println("\n- An error ocurred while reading the file. \n", err)
+		return nil
+    }
+	return csvLines
+}
+
+func ParseCSVDataToItemsList(csvLines [][]string) (listOfItems []Item ) { 
+	// Convert csv lines to a generic item structure and append them to the array of items
+    for _, line := range csvLines {
+		newItem := Item{
+            Id: line[0],
+            Title: line[1],
+        }
+        listOfItems = append(listOfItems, newItem)
+        log.Println(newItem.Id + " " + newItem.Title + " ")
+    }
+	return 
+}
+
+var csvFileName string = "test.csv"
+
 func TestWriteDataToCSVFile(t *testing.T) {
 	type args struct {
 		fileName string
@@ -22,16 +56,24 @@ func TestWriteDataToCSVFile(t *testing.T) {
 		args args
 	}{
 		{
-			name: "",
+			name: "Write struct Item to csv",
 			args: args{
-				fileName: "test.csv", 
+				fileName: csvFileName, 
 				items: Items,
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			// write the data
 			WriteDataToCSVFile(tt.args.fileName, tt.args.items)
+			// check if data was written properly
+			csvLines := GetDataFromCSVFile(tt.args.fileName)
+			listOfItems :=  ParseCSVDataToItemsList(csvLines)   
+			log.Print(listOfItems)
+			if len(listOfItems) != len(Items) { // is the same length
+				t.Errorf("Expected %v items but instead got %v!", len(Items), len(listOfItems))
+			}
 		})
 	}
 }
