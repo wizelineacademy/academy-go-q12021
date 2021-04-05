060b7657fc00b023d73875027a2b62504cfacd23
Ivan Ibarra Pacheco <bytepacheco@gmail.com>
2021-04-04 19:51:19 -0500

    Add some cool stuff to UI

--- frontend/main.go
+++ frontend/main.go
@@ -172,7 +172,10 @@ func RenderMovieById(w http.ResponseWriter, r *http.Request) {
 
 func RenderDefault(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
-		RenderNotFoundPage(w)
+		tmpl := template.Must(template.ParseFiles("html/index.html"))
+		if err := tmpl.Execute(w, nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 		return
 	}
 	if r.URL.Path != "/" {
